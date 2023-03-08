7fd099904b79fef725e4796493f78bebbae137e7
Hlushcko <mr.vproen7shchsgjg@gmail.com>
2023-03-08 22:49:30 +0200

    updated some ui element and was making ranobe list

--- app/src/main/java/com/ranobeua/authentication/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/ranobeua/authentication/fragments/RegistrationFragment.kt
@@ -1,14 +1,10 @@
 package com.ranobeua.authentication.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.ranobeua.R
 
 
-class SigninFragment : Fragment() {
+class RegistrationFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
